3d99b42e65d2f3612518def601e8b341a40f694e
Matt Chvatal <mdchvatal@gmail.com>
2021-02-08 20:18:45 -0500

    Added comment for clarity.

--- VotingBox.java
+++ VotingBox.java
@@ -1,3 +1,13 @@
+/* VotingBox.java
+ * 
+ * Program to clear out hanging chads from the VotingBox world. 
+ * Karel checks ballots to see if, after a vote is registered, there are any beepers hanging about.
+ * He cleans them up and stops once he's moved through them all from West to East.
+ * 
+ */
+
+
+
 import stanford.karel.*;
 
 public class VotingBox extends SuperKarel {
