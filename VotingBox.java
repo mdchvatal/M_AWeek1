da51c1a34ed11c4b8eabec0d4a0f221e51a8cc88
Matt Chvatal <mdchvatal@gmail.com>
2021-02-08 19:11:23 -0500

    Upload completed assignment VotingBox

--- VotingBox.java
+++ VotingBox.java
@@ -3,8 +3,38 @@ import stanford.karel.*;
 public class VotingBox extends SuperKarel {
 
 	public void run() {
+		while (frontIsClear()) {
+			move();
+			checkVote();
+			move();
+		}
 		
 				
+	}
+	
+	private void checkVote() {
+		if (!beepersPresent()) {
+			checkForChads();
+		}
+	}
+	
+	private void checkForChads() {
+		turnLeft();
+		move();
+		while (beepersPresent()) {
+			pickBeeper();
+		}
+		turnRight();
+		turnRight();
+		move();
+		move();
+		while (beepersPresent()) {
+			pickBeeper();
+		}
+		turnLeft();
+		turnLeft();
+		move();
+		turnRight();
 	}
 	
 		
